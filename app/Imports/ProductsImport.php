16d4a0d22e136844069e5cc71000016ee00fac45
Dyago Matheus <dyagomatheus123@gmail.com>
2021-05-24 13:25:42 -0300

    Importar produtos

---
+++ app/Imports/ProductsImport.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Imports;
+
+use App\Models\Product;
+use Maatwebsite\Excel\Concerns\ToModel;
+
+class ProductsImport implements ToModel
+{
+    /**
+    * @param array $row
+    *
+    * @return \Illuminate\Database\Eloquent\Model|null
+    */
+    public function model(array $row)
+    {
+        foreach ($row as $item) {
+            $product = new Product();
+            $product->name = $item;
+            $product->save();
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ClientController;
 use App\Http\Controllers\DashboardController;
 
+use App\Imports\ProductsImport;
+use Maatwebsite\Excel\Facades\Excel;
 
 Route::resource('client', ClientController::class);
 
@@ -15,5 +17,10 @@ Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['au
 Route::get('/devolution/edit/{id}', [DashboardController::class, 'devolutionEdit'])->middleware(['auth'])->name('devolution.edit');
 Route::post('/devolution/update/{id}', [DashboardController::class, 'devolutionUpdate'])->middleware(['auth'])->name('devolution.update');
 Route::get('/devolution/show/{id}', [DashboardController::class, 'devolutionShow'])->middleware(['auth'])->name('devolution.show');
-
+Route::get('import-models', function () {
+    return view('import');
+});
+Route::post('import-models', function () {
+    Excel::import(new ProductsImport, request()->file('products'));
+});
 require __DIR__.'/auth.php';
